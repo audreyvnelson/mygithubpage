ab20a8bdcbaa3e5c4cb571c3649c8b5f3a7f87cb
audreyvnelson <audreyvnelson@gmail.com>
2014-11-07 17:40:23 -0500

    Testing Angular

---
+++ data/projects.js
@@ -0,0 +1,15 @@
+
+function getProjectData() {
+	var projects = [
+		{
+			
+					'name': "Project 1",
+					'code': "something.com"
+				},
+				{
+					'name': "Project 2",
+					'code': "somethingelse.com"
+				}
+	]
+	return projects;
+}
